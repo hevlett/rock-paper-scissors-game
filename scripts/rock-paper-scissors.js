5faed1d4225b49b36f99379000b8205063fc02c1
GERALD TORMES <136874080301@ncrdeped2.onmicrosoft.com>
2024-05-13 21:50:52 +0800

    edit js file

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -49,12 +49,10 @@ let score = JSON.parse(localStorage.getItem('score')) ?? { win: 0, lose: 0, tie:
 
         updateScoreElement();
         document.querySelector('.js-result').innerHTML = result;
-        document.querySelector('.js-move').innerHTML = `
-        You Pick
-        <img src="images/${playerMove}-emoji.png" class="move-icon"> 
-        <img src="images/${computerMove}-emoji.png" class="move-icon"> 
-        Computer Pick
-      `;
+        document.querySelector('.js-move').innerHTML = `You Pick
+        <img src="./images/${playerMove}-emoji.png" class="move-icon"> 
+        <img src="./images/${computerMove}-emoji.png" class="move-icon"> 
+        Computer Pick`;
 
 
 //       alert(`You Picked ${playerMove} The Computer Picked ${computerMove} The Result is ${result}
